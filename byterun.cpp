159e27fe258b9301a51298399f41476cb66641af
feliposz <feliposz@gmail.com>
2016-11-24 14:32:37 -0300

    Multiple types, if statement, comparison operator

--- byterun.cpp
+++ byterun.cpp
@@ -7,11 +7,22 @@ using namespace std;
 // Instruction set
 enum class OpCode
 {
-    LOAD_VALUE,
-    ADD_TWO_VALUES,
-    PRINT_ANSWER,
-    STORE_NAME,
-    LOAD_NAME
+    LOAD_CONST,
+    STORE_FAST,
+    LOAD_FAST,
+    COMPARE_OP,
+    POP_JUMP_IF_FALSE,
+    RETURN_VALUE,
+};
+
+enum CompareOperator
+{
+    EQUAL,
+    NOT_EQUAL,
+    LESS_THAN,
+    GREATER_THAN,
+    LESS_THAN_OR_EQUAL,
+    GREATER_THAN_OR_EQUAL,
 };
 
 struct Instruction
@@ -20,88 +31,265 @@ struct Instruction
     int Argument;
 };
 
+enum class ObjectType
+{
+    NONE,
+    INTEGER,
+    FLOAT,
+    STRING,
+};
+
+struct Object
+{
+    ObjectType Type;
+    struct
+    {
+        int Integer;
+        float Float;
+        string String;
+    };
+    Object() : Type {ObjectType::NONE} {}
+    Object(int i) : Type {ObjectType::INTEGER}, Integer {i} {}
+    Object(float f) : Type {ObjectType::FLOAT}, Float {f} {}
+    Object(string s) : Type {ObjectType::STRING} { String = s; }
+};
+
 struct Program
 {
     vector<Instruction> Instructions;
-    vector<int> NumberConstants;
+    vector<Object> Constants;
     vector<string> Names;
 };
 
+void ReturnObject(Object o)
+{
+    switch (o.Type)
+    {
+    case ObjectType::NONE:
+        break;
+    case ObjectType::INTEGER:
+        {
+            cout << "    Return (Integer): " << o.Integer << endl;
+        }
+        break;
+    case ObjectType::FLOAT:
+        {
+            cout << "    Return (Float): " << o.Float << endl;
+        }
+        break;
+    case ObjectType::STRING:
+        {
+            cout << "    Return (String): \"" << o.String << "\"" << endl;
+        }
+        break;
+    default:
+        {
+            cerr << "Not implemented!";
+        }
+        break;
+    }
+}
+
+bool CompareObject(CompareOperator Operator, Object Left, Object Right)
+{
+    bool Result = false;
+    if (Left.Type == ObjectType::INTEGER && Right.Type == ObjectType::INTEGER)
+    {
+        switch (Operator)
+        {
+        case EQUAL: Result = (Left.Integer == Right.Integer); break;
+        case NOT_EQUAL: Result = (Left.Integer != Right.Integer); break;
+        case LESS_THAN: Result = (Left.Integer < Right.Integer); break;
+        case GREATER_THAN: Result = (Left.Integer > Right.Integer); break;
+        case LESS_THAN_OR_EQUAL: Result = (Left.Integer <= Right.Integer); break;
+        case GREATER_THAN_OR_EQUAL: Result = (Left.Integer >= Right.Integer); break;
+        default: break; // TODO: log invalid operator
+        }
+    }
+    else if (Left.Type == ObjectType::FLOAT && Right.Type == ObjectType::FLOAT)
+    {
+        switch (Operator)
+        {
+        case EQUAL: Result = (Left.Float == Right.Float); break;
+        case NOT_EQUAL: Result = (Left.Float != Right.Float); break;
+        case LESS_THAN: Result = (Left.Float < Right.Float); break;
+        case GREATER_THAN: Result = (Left.Float > Right.Float); break;
+        case LESS_THAN_OR_EQUAL: Result = (Left.Float <= Right.Float); break;
+        case GREATER_THAN_OR_EQUAL: Result = (Left.Float >= Right.Float); break;
+        default: break; // TODO: log invalid operator
+        }
+    }
+    else if (Left.Type == ObjectType::STRING && Right.Type == ObjectType::STRING)
+    {
+        switch (Operator)
+        {
+        case EQUAL: Result = (Left.String == Right.String); break;
+        case NOT_EQUAL: Result = (Left.String != Right.String); break;
+        case LESS_THAN: Result = (Left.String < Right.String); break;
+        case GREATER_THAN: Result = (Left.String > Right.String); break;
+        case LESS_THAN_OR_EQUAL: Result = (Left.String <= Right.String); break;
+        case GREATER_THAN_OR_EQUAL: Result = (Left.String >= Right.String); break;
+        default: break; // TODO: log invalid operator
+        }
+    }
+    else
+    {
+        cerr << "Type Mismatch!" << endl;
+    }
+    return Result;
+}
+
+bool IsTrueValue(Object o)
+{
+    bool Result = false;
+    switch (o.Type)
+    {
+    case ObjectType::NONE:
+        {
+            Result = false;
+        }
+        break;
+    case ObjectType::INTEGER:
+        {
+            Result = o.Integer != 0;
+        }
+        break;
+    case ObjectType::FLOAT:
+        {
+            Result = o.Integer != 0.0f;
+        }
+        break;
+    case ObjectType::STRING:
+        {
+            Result = o.String.size() != 0;
+        }
+        break;
+    }
+    return Result;
+}
+
 void Run(const Program &p, const bool debug)
 {
-    vector<int> Stack;
-    unordered_map<string, int> Variables;
 
-    for (auto Instruction : p.Instructions)
+    vector<Object> Stack;
+    unordered_map<string, Object> Variables;
+
+    auto InstructionPointer = 0;
+
+    while (InstructionPointer < p.Instructions.size())
     {
+
+        auto Instruction = p.Instructions[InstructionPointer];
+
+        // TODO: Check stack underflow!
+        // TODO: Check undefined variables!
+
         switch (Instruction.Code)
         {
-        case OpCode::LOAD_VALUE:
+        case OpCode::LOAD_CONST:
             {
                 int ConstantIndex = Instruction.Argument;
-                if (debug) cout << "LOAD_VALUE " << ConstantIndex << endl;
-                int Value = p.NumberConstants[ConstantIndex];
+                if (debug) cout << "LOAD_CONST " << ConstantIndex << endl;
+                Object Value = p.Constants[ConstantIndex];
                 Stack.push_back(Value);
             }
             break;
-        case OpCode::STORE_NAME:
+        case OpCode::STORE_FAST:
             {
                 int NameIndex = Instruction.Argument;
                 if (debug) cout << "STORE_NAME " << NameIndex << endl;
-                int Value = Stack.back();
+                Object Value = Stack.back();
                 Stack.pop_back();
                 Variables[p.Names[NameIndex]] = Value;
             }
             break;
-        case OpCode::LOAD_NAME:
+        case OpCode::LOAD_FAST:
             {
                 int NameIndex = Instruction.Argument;
                 if (debug) cout << "LOAD_NAME " << NameIndex << endl;
-                int Value = Variables[p.Names[NameIndex]];
+                Object Value = Variables[p.Names[NameIndex]];
                 Stack.push_back(Value);
             }
             break;
-        case OpCode::ADD_TWO_VALUES:
+        case OpCode::COMPARE_OP:
             {
-                if (debug) cout << "ADD_TWO_VALUES" << endl;
-                int First = Stack.back();
+                CompareOperator Operator = (CompareOperator)Instruction.Argument;
+                if (debug) cout << "COMPARE_OP " << Operator << endl;
+                Object Right = Stack.back();
                 Stack.pop_back();
-                int Second = Stack.back();
+                Object Left = Stack.back();
                 Stack.pop_back();
-                Stack.push_back(First + Second);
+                bool Result = CompareObject(Operator, Left, Right);
+                Stack.push_back(Result ? Object {1} : Object {0});
             }
             break;
-        case OpCode::PRINT_ANSWER:
+        case OpCode::POP_JUMP_IF_FALSE:
             {
-                if (debug) cout << "PRINT_ANSWER" << endl;
-                int Value = Stack.back();
+                if (debug) cout << "POP_JUMP_IF_FALSE" << endl;
+                Object Value = Stack.back();
                 Stack.pop_back();
-                cout << Value << endl;
+                if (!IsTrueValue(Value))
+                {
+                    InstructionPointer = Instruction.Argument;
+                    continue;
+                }
+            }
+            break;
+        case OpCode::RETURN_VALUE:
+            {
+                if (debug) cout << "RETURN_VALUE" << endl;
+                Object Value = Stack.back();
+                Stack.pop_back();
+                ReturnObject(Value);
+                InstructionPointer = p.Instructions.size();
+                // TODO: Find a better way to gracefully return...
+            }
+            break;
+        default:
+            {
+                // TODO: log invalid instruction
             }
             break;
         }
+
         if (debug) cout << "    Stack size: " << Stack.size() << endl;
+
+        InstructionPointer++;
     }
 }
 
 int main()
 {
-    // a = 1
-    // b = 2
-    // print a + b
-    const Program p {
+    // x = 3
+    // if x < 5:
+    //     return 'yes'
+    // else:
+    //     return 'no'
+    const Program p  {
         {
-            {OpCode::LOAD_VALUE, 0},
-            {OpCode::STORE_NAME, 0},
-            {OpCode::LOAD_VALUE, 1},
-            {OpCode::STORE_NAME, 1},
-            {OpCode::LOAD_NAME, 0},
-            {OpCode::LOAD_NAME, 1},
-            {OpCode::ADD_TWO_VALUES},
-            {OpCode::PRINT_ANSWER},
+            {OpCode::LOAD_CONST, 1},
+            {OpCode::STORE_FAST, 0},
+            {OpCode::LOAD_FAST, 0},
+            {OpCode::LOAD_CONST, 2},
+            {OpCode::COMPARE_OP, LESS_THAN},
+            {OpCode::POP_JUMP_IF_FALSE, 8},
+            {OpCode::LOAD_CONST, 3},
+            {OpCode::RETURN_VALUE},
+            {OpCode::LOAD_CONST, 4}, // << JUMP
+            {OpCode::RETURN_VALUE},
+            {OpCode::LOAD_CONST, 0},
+            {OpCode::RETURN_VALUE},
         },
-        {1, 2},
-        {"a", "b"}
+        {
+            Object {},
+            Object {3},
+            Object {5},
+            Object {"yes"},
+            Object {"no"},
+        },
+        {
+            "x"
+        }
     };
 
     Run(p, true);
